910813176f8cba20afdae3119989474c6fc432c2
Ryan Cajigal <rcajigal97@gmail.com>
2017-09-04 21:58:18 -0700

    Comments

--- SiteData.cpp
+++ SiteData.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 static int callback(void * used, int argc, char ** argv, char ** szColName) {
+  // set pointer structs values
   select_wrapper * encSelect = (select_wrapper *) used;
   encSelect->password = string(argv[0]);
 
@@ -16,6 +17,7 @@ static int callback(void * used, int argc, char ** argv, char ** szColName) {
 }
 
 string SiteData::getPassword(char key) {
+  // decrypt passowrd using XOR cipher backwards
   string password = "";
   for (unsigned int i = 0; i < encPass.size(); i++) {
     password += encPass[i] ^ (int(key) + i) % 255;
@@ -24,6 +26,7 @@ string SiteData::getPassword(char key) {
 }
 
 string SiteData::encodePassword(string password, char key) {
+  // encrypt passowrd using XOR cipher
   string encryptPW = "";
   for (unsigned int i = 0; i < password.size(); i++) {
     encryptPW += password[i] ^ (int(key) + i) % 255;
@@ -32,20 +35,23 @@ string SiteData::encodePassword(string password, char key) {
 }
 
 void SiteData::insertPass(sqlite3 * db) {
+  // check if database exists
   if (!db) {
     return;
   }
 
+  // create prepared statement
   sqlite3_stmt * stmt;
   const char * pzTest;
   const char * szSQL;
-  string insert = "INSERT INTO passwords (id, website, user, password) values (?, ?, ?, ?)";
 
+  string insert = "INSERT INTO passwords (id, website, user, password) values (?, ?, ?, ?)";
   szSQL = insert.c_str();
 
   int rc = sqlite3_prepare(db, szSQL, strlen(szSQL), &stmt, &pzTest);
 
   if (rc == SQLITE_OK) {
+    // prepare params and bind to prepared statement
     const char * websiteStmt = site.c_str();
     const char * userStmt = user.c_str();
     const char * passwordStmt = encPass.c_str();
@@ -55,32 +61,35 @@ void SiteData::insertPass(sqlite3 * db) {
     sqlite3_bind_text(stmt, 3, userStmt, strlen(userStmt), 0);
     sqlite3_bind_text(stmt, 4, passwordStmt, strlen(passwordStmt), 0);
 
+    // execute
     sqlite3_step(stmt);
     sqlite3_finalize(stmt);
   }
 }
 
 void SiteData::selectPass(sqlite3 * db) {
-  char * szErrMsg = 0;
-
+  // check if database exists
   if (!db) {
     return;
   }
 
+  // create prepared statement
+  char * szErrMsg = 0;
   const char * pSQL;
+
   string select = "SELECT password FROM passwords WHERE website = '" + site +
     "' AND user = '" + user + "'";
   pSQL = select.c_str();
 
+  // use struct to obtain select data
   select_wrapper result;
   int rc = sqlite3_exec(db, pSQL, callback, &result, &szErrMsg);
   
+  // check for error else set encrypted password
   if (rc != SQLITE_OK) {
     cerr << "Error: " << szErrMsg << endl;
     sqlite3_free(szErrMsg);
   } else {
-    cout << result.password << endl;
     encPass = result.password;
-    //getPassword('x');
   }
 }
--- SiteData.h
+++ SiteData.h
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// struct to be used for callback to obtain select statement values
 struct select_wrapper {
   string password;
 };
@@ -14,27 +15,35 @@ struct select_wrapper {
 class SiteData {
 
   private:
-    string encPass;
+    string encPass; // encrypted password
 
+    // function to encode a password using an XOR cipher along with user 
+    // inputted char key
     string encodePassword(string password, char key);
     
+    // function to retrieve password
     void selectPass(sqlite3 * db);
 
   public:
-    string site;
-    string user;
+    string site;  // website
+    string user;  // username or email
 
+    // constructor for input
     SiteData(string site, string user, string unencPass, char key) : site(site), 
       user(user) {
       encPass = encodePassword(unencPass, key);
     }
 
+    // constructor for retrieval
     SiteData(string site, string user, sqlite3 * db) : site(site), user(user) {
       selectPass(db);
     }
 
+    // function to decode a password using an XOR cipher which only results in the
+    // correct password if the correct key is used
     string getPassword(char key);
 
+    // function to insert an encrypted password into an sqlite database
     void insertPass(sqlite3 * db);
 
 };
